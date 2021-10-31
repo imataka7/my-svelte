ba9bdec55e28a61d0a9846f8f83038b458343378
imataka7 <imataka7@gmail.com>
2021-11-01 01:57:16 +0900

    Add router

---
+++ src/About.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { link } from 'svelte-spa-router';
+</script>
+
+<h1>This is a about page</h1>
+<a href="/" use:link>Home</a>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		/* text-transform: uppercase; */
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
---
+++ src/Home.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { link } from 'svelte-spa-router';
+	export let name = 'HOST3';
+</script>
+
+<h1>Hello {name}!</h1>
+<a href="/about" use:link>About</a>
+<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
